03569b03e1ffc34af916c6ffc512f9b49336b81d
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-16 21:22:47 +0700

    Update gczifabotz.js

--- plugins/gczifabotz.js
+++ plugins/gczifabotz.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let pp = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=Group'
 let botol = global.botwm
 let str = `
-*Group zifabotz1 📮: https://chat.whatsapp.com/I8Q4oJVw8buHhIgMH5iVAv*
+*Group Mikazu Botz : https://bit.ly/3ax9GjJ*
 `.trim()
-conn.sendButton(m.chat, str, `© zifabotz`, `⋮☰ Menu`, `.menu`, m)
+conn.sendButton(m.chat, str, `Mikazu Md By Asyraf 6Youu`, `OWNER MIKAZU`, `.owner`, m)
 }
 handler.help = ['zifa1']
 handler.tags = ['info']
