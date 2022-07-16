758deec765424abf51dfccbb300e906d941404a9
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-16 21:42:01 +0700

    Update broadcastlist.js

--- plugins/broadcastlist.js
+++ plugins/broadcastlist.js
@@ -10,7 +10,7 @@ let handler  = async (m, { conn, text }) => {
     itemCount : 7282,
     status: 1,
     surface : 1,
-    message: `Broadcast Zifabot,`, 
+    message: `Broadcast Mikazu,`, 
     orderTitle: `▮Menu ▸`,
     thumbnail: await (await fetch(fla + 'BZ')).buffer(), //Gambarnye
     sellerJid: '0@s.whatsapp.net' 
@@ -21,7 +21,7 @@ let handler  = async (m, { conn, text }) => {
   let content = (/bc|broadcast/i.test(text) ? text : text)
   for (let id of chats) conn.relayWAMessage(conn.prepareMessageFromContent(id, {
         "listMessage": {
-          "title": `───────[ *BROADCAST ZIFABOTZ* ]───────`.trim(),
+          "title": `───────[ *BROADCAST MIKAZU* ]───────`.trim(),
           "description": content.trim(),
           "footerText": 
 global.botdate,
